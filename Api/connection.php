782e1b27237bc884ab507d69888f6e592e3b2959
Hadi-AL-Saden <hadialsaden@gmail.com>
2022-05-30 17:36:49 -0800

    api

---
+++ Api/connection.php
@@ -0,0 +1,18 @@
+<?php
+$servername='localhost';
+$dbusername='root';
+$password='';
+$database='api';
+
+// Create connection
+$conn = mysqli_connect($servername,$dbusername, $password,$database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+else
+{
+}
+
+?>
